11c5a9133b603e51a50529ff678e566ce4b9ee97
Polatcan Gürbüz <427638@ogr.ktu.edu.tr>
2025-05-17 15:50:36 +0300

    Topuz Rotation fixed

--- Assets/Scripts/TopuzMovement.cs
+++ Assets/Scripts/TopuzMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class TopuzMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 mouseStartPos;
+    private bool isDragging = false;
+
+    public float sensitivity = 0.3f; 
+
+    public  float currentX = 0f;
+    public float currentZ = 0f;
+
+    public float deltaX;
+    public float deltaY;
+
+    public Vector3 currentMousePos;
+
+    private void OnMouseDown()
     {
-        
+        mouseStartPos = Input.mousePosition;
+        isDragging = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        isDragging = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!isDragging) return;
+
+        currentMousePos = Input.mousePosition;
+
+        deltaY = currentMousePos.y - mouseStartPos.y; 
+        deltaX = currentMousePos.x - mouseStartPos.x; 
+
+        currentX -= deltaY * sensitivity;
+        currentZ -= deltaX * sensitivity;
+
+        currentX = Mathf.Clamp(currentX, -20f, 30f);
+        currentZ = Mathf.Clamp(currentZ, -20f, 20f);
+
+        transform.localEulerAngles = new Vector3(currentX, 0f, -currentZ);
+       
+        mouseStartPos = currentMousePos;
     }
 }
